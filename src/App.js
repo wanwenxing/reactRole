e33b926507d50661c611088667f4dbd89150825d
wan <15700492556@163.com>
2020-07-28 17:14:10 +0800

    初始化文件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+
+
+//引入组件
+import Login from './pages/Login'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +24,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Login></Login>
     </div>
   );
 }
